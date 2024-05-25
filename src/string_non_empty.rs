83c43bfa4cd520c31fa863719c324c5fa15ba5d7
Rob Ede <robjtede@icloud.com>
2024-05-26 00:44:14 +0100

    fix: option_string_non_empty for null values

--- src/string_non_empty.rs
+++ src/string_non_empty.rs
@@ -16,11 +16,11 @@ pub fn string_non_empty<'a, D: Deserializer<'a>>(de: D) -> Result<String, D::Err
 
 /// Trims string during deserialization, returning `None` if it ends up empty.
 pub fn option_string_non_empty<'a, D: Deserializer<'a>>(de: D) -> Result<Option<String>, D::Error> {
-    String::deserialize(de).map(|val| {
-        Some(val.trim())
-            .filter(|val| !val.is_empty())
-            .map(ToOwned::to_owned)
-    })
+    match Option::<String>::deserialize(de)? {
+        None => Ok(None),
+        Some(val) if val.trim().is_empty() => Ok(None),
+        Some(val) => Ok(Some(val.trim().to_owned())),
+    }
 }
 
 #[cfg(test)]
@@ -84,6 +84,10 @@ mod tests {
             }
         }
 
+        assert_eq!(
+            Foo::none(),
+            serde_json::from_str(r#"{ "foo": null }"#).unwrap(),
+        );
         assert_eq!(
             Foo::none(),
             serde_json::from_str(r#"{ "foo": "" }"#).unwrap(),
